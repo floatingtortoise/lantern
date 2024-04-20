ee4612e80ab53c80f6c0debeed328c490ece6d42
EulaFRL <97711833+EulaFRL@users.noreply.github.com>
2024-04-20 18:04:03 -0400

    newest prompts
    substituted older versions of quiz_routes_and_services and gpt3_service

--- backend/app/routes/quiz_routes_and_services.py
+++ backend/app/routes/quiz_routes_and_services.py
@@ -1,24 +1,27 @@
-from flask import Blueprint, request, jsonify, current_app, Flask
+# prompt engineering and processing response into formated quiz questions
+from flask import Blueprint, request, jsonify, current_app, make_response
 from werkzeug.utils import secure_filename
 from ..services.text_extraction import extract_text
+# from ..services.gpt3_service import ask_gpt3
 from ..services.gpt3_service import ask_gpt3, ask_gpt3_follow
 import os
 
-
 quiz_bp = Blueprint('quiz', __name__)
 
+# @todo: update file type restrictions
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg'}
+
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@quiz_bp.route('/create_quiz', methods=['GET'])
-def create_quiz():
-    message = request.form.get('message', '')  # TODO: extract instructions from frontend
-    # message = 'Generate quiz questions for my astronomy midterm revision.' # dummy
 
+@quiz_bp.route('/create_quiz', methods=['POST'])
+def create_quiz():
+    message = request.form.get('message', '')  # Extract text message from form data
+
+    files = request.files.getlist('files')  # Allows for multiple files
     material = []
-    files = request.files.getlist('files')  # TODO: extract documents uploaded  from frontend
-    # files = [] # dummy
     for file in files:
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -26,32 +29,33 @@ def create_quiz():
             file.save(filepath)
             extracted_text = extract_text(filepath)
             material.append(extracted_text)
-    materials = "\n".join(material)
+    materials = "\n".join(material)  # concatenation separated by \n
 
-    prompt = f"Instructions: {message} \n Materials: {materials}"
+    prompt = ""
+    if (len(files), len(message)) == (0, 0):
+        return make_response('Internal Server Error', 500)
+    elif len(files) == 0:  # no materials, only topic description
+        prompt = f"They want to learn about the topic: {message}."
+    elif len(message) == 0:  # no topic description, only materials
+        prompt = f"Here are some materials they want to learn about: {materials}"
+    else:  # both materials and topic descriptions are provided
+        prompt = f"They want to focus the quiz on the topic: {message}. " \
+                 f"Please restrict the scope of the quiz to information in this material: {materials}"
+
     gpt3_response = ask_gpt3(prompt)
-    return jsonify(gpt3_response)
+    return jsonify({'quiz_question(s)': gpt3_response, 'prompt': prompt})
+
 
 @quiz_bp.route('/follow_up', methods=['POST'])
-# @app.route('/follow_up', methods=['GET'])
 def follow_up():
-    message = request.form.get('message', '')  # TODO: extract instructions from frontend
-    
-    material = []
-    files = request.files.getlist('files')  # TODO: extract documents uploaded  from frontend
-    for file in files:
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
-            file.save(filepath)
-            extracted_text = extract_text(filepath)
-            material.append(extracted_text)
-    materials = "\n".join(material) # concatenation separated by \n
+    question = request.form.get('question', '')  # previous question
+    answer = request.form.get('answer', '')  # user answer
+    message = request.form.get('message', '')  # previous user message
+    previous_questions = request.form.get('previousQuestions', '')
 
-    question = request.form.get('question', '')  # TODO: extract corresponding quiz question from cashed (?) data
+    prompt = f'{message} ' \
+             f'The previous question they got wrong is "{question}", to which the user chose "{answer}". ' \
+             f'Other questions you have previously asked: "{previous_questions}"'
 
-    answer = request.form.get('answer', '')  # TODO: extract user's response from frontend
-
-    prompt = f"Instructions: {message} \n Materials: {materials} \n Question: {question} \n Answer: {answer}"
     gpt3_response = ask_gpt3_follow(prompt)
-    return jsonify({gpt3_response})
+    return jsonify({'follow_up_question': gpt3_response})
--- backend/app/services/gpt3_service.py
+++ backend/app/services/gpt3_service.py
@@ -3,9 +3,29 @@ import os
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
+# def ask_gpt3(prompt):
+#     response = openai.chat.completions.create(
+#         model="gpt-3.5-turbo",
+#         messages=[{"role": "user", "content": prompt}],
+#         n=1,                    # How many chat completion choices to generate for each input message
+#         temperature=0.5,        # controls the randomness of the output
+#         #@todo: cap the file size and the response size. the max of this model is 16385 tokens
+#         max_tokens= 2000,        # of the generated response
+#         top_p=1.0,              # also controls the randomness/variability
+#         frequency_penalty=0.0,  # zero penalty for repeated words
+#         presence_penalty=0.0    # similarly
+#     )
+#     return response.choices[0].message.content
+
 def ask_gpt3(prompt):
     system_content = """
-        You are an assistant who helps children learn by generating quiz questions. You will be provided with 1) instructions about what they want to learn, and 2) potential study materials about the topic. Generate a list of 5 multiple-choice questions - each question has a list of 4 choices, with only the first choice being the correct answer. Each question follows the format: {'question': '...', 'choices': ['...', '...', '...', '...']}
+        Your task is to generate 5 quiz questions for young children to help them learn concepts or skills.
+        You will be provided with either the specifications of the topic or the material, or both.
+        When both are provided, please focus each question only on the topic, if provided.
+        Regarding the materials, it only serves as a reference for the scope the questions on that topic should be limited to.
+        When you generate the quiz, please take into consideration the key concepts and skills related to the topic.
+        Each question has a list of 4 choices, with only the first choice being the correct answer. Each question 
+        follows the format: {'question': '...', 'choices': ['...', '...', '...', '...']}
     """
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -13,7 +33,6 @@ def ask_gpt3(prompt):
                     {"role": "user", "content": prompt}],
         n=1,                    # How many chat completion choices to generate for each input message
         temperature=0.5,        # controls the randomness of the output
-        #@todo: cap the file size and the response size. the max of this model is 16385 tokens
         max_tokens= 2000,        # of the generated response
         top_p=1.0,              # also controls the randomness/variability
         frequency_penalty=0.0,  # zero penalty for repeated words
@@ -23,7 +42,16 @@ def ask_gpt3(prompt):
 
 def ask_gpt3_follow(prompt):
     system_content = """
-        You are an assistant who helps children learn by asking follow-up questions based on their previous answer to a quiz question that encourages them to specify on their understanding to the topic. You will be provided with 1) the initial ask of the user for quiz generation, 2) potential study materials about the topic to study, 3) the quiz question generated, and 4) user's answer to the quiz question. Generate a multiple-choice question with a list of 4 choices - only the first choice should be the correct answer. Follow the format: {'question': '...', 'choices': ['...', '...', '...', '...']}
+        Your task is to generate one follow-up question for a child based on another question they got wrong.
+        You will be provided with the relevant topics or materials the child is studying, as well as other questions
+        you have already asked.
+        In generating the follow-up question, please stick to the scope of those topics and materials.
+        Meanwhile, please consider the possible reasons why they got it wrong. 
+        Try to generate a question that can invoke reflections on the previous question, and avoid generating 
+        a question that they most likely will also get wrong due to certain lack of information you identified 
+        from their previous answer.
+        The question has a list of 4 choices, with only the first choice being the correct answer. 
+        Your response should follow the format: {'question': '...', 'choices': ['...', '...', '...', '...']}
     """
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -31,7 +59,6 @@ def ask_gpt3_follow(prompt):
                     {"role": "user", "content": prompt}],
         n=1,                    # How many chat completion choices to generate for each input message
         temperature=0.5,        # controls the randomness of the output
-        #@todo: cap the file size and the response size. the max of this model is 16385 tokens
         max_tokens= 2000,        # of the generated response
         top_p=1.0,              # also controls the randomness/variability
         frequency_penalty=0.0,  # zero penalty for repeated words
